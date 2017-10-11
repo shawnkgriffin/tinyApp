8bb251267b0e9878a70a11d6fc8995ac9454a968
Shawn Griffin <shawn@shawngriffin.com>
2017-10-11 16:12:24 -0700

    Have now implemented the logout button in the header.

--- express_server.js
+++ express_server.js
@@ -1,15 +1,20 @@
-//requires and globals
+//requires
 const bodyParser = require("body-parser");
 var express = require("express");
-var cookieParser = require('cookie-parser')
+var cookieParser = require("cookie-parser");
+
+//start up express, set up the components we are using.
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
+// set the view engine to ejs
+app.set("view engine", "ejs");
 
 var PORT = process.env.PORT || 8080; // default port 8080
-const tinyURLLength = 6;
 
+//GLOBALS
 // Temporary database.
+const tinyURLLength = 6;
 var urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -17,7 +22,7 @@ var urlDatabase = {
 
 // Functions and other worker items remember to refactor these to a module.
 
-// generate 6 random alphanumeric characters
+// generate length random alphanumeric characters
 function generateRandomString(length) {
   const validChars =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -28,59 +33,47 @@ function generateRandomString(length) {
   return random;
 }
 
-// set the view engine to ejs
-app.set("view engine", "ejs");
-
-
+// GET HANDLERS
 // use res.render to load up an ejs view file
-
+// GET /
 app.get("/", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
 
-  // Cookies that have not been signed
-  console.log('Cookies: ', req.cookies)
-  
+  // Cookies check
+  console.log("Cookies: ", req.cookies);
+
+  // render to main page that shows all URLS
   res.render("pages/urls_index", templateVars);
 });
 
+// GET /URLS
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("pages/urls_index", templateVars);
 });
 
+// GET URLS/NEW
+// User wants to get a new small URL
 app.get("/urls/new", (req, res) => {
-  res.render("pages/urls_new");
-});
-
-app.post("/urls", (req, res) => {
-  const key = generateRandomString(tinyURLLength);
-  urlDatabase[key] = "http://" + req.body.longURL;
-
-  res.redirect("/urls/" + key);
-});
-
-//change a Long URL
-app.post("/urls/:id/change", (req, res) => {
-  urlDatabase[req.params.id] = req.body.newLongURL;
-
-  res.redirect("/");
-});
-
-// Login
-app.post("/login", (req, res) => {
+  let templateVars = {
+    username: req.cookies["username"]
+  };
   
-  console.log('Userid ',req.body.userID);
-
-  res.cookie('username', req.body.userID);
-
-  res.redirect("/urls");
+  res.render("pages/urls_new", templateVars);
 });
 
-//show the URL and allow the user to change the URL. 
+//show the URL and allow the user to change the URL.
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id]
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
   };
   res.render("pages/urls_show", templateVars);
 });
@@ -89,22 +82,65 @@ app.get("/urls/:id", (req, res) => {
 app.get("/urls/:id/delete", (req, res) => {
   let shortURL = req.params.id;
   delete urlDatabase[shortURL];
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("pages/urls_index", templateVars);
 });
 
-// What would happen if a client requests a non-existent shortURL?
-// What happens to the urlDatabase when the server is restarted?
-// Should your redirects be 301 or 302 - What is the difference?
+//GET /u/:shortURL do the redirection to the longURL
 app.get("/u/:shortURL", (req, res) => {
+  //TODO Check parameters
+  // What would happen if a client requests a non-existent shortURL?
+  // What happens to the urlDatabase when the server is restarted?
+  // Should your redirects be 301 or 302 - What is the difference?
+
   let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-// about page
+// GET /about - Added an About page for fun.
 app.get("/about", function(req, res) {
-  res.render("pages/about");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("pages/about", templateVars);
 });
+
+
+// POSTS
+
+// POST /urls when we get a new tiny URL from /urls/new
+// create a random string (key) then redirect to /urls/key to allow user to view or change.
+app.post("/urls", (req, res) => {
+  const key = generateRandomString(tinyURLLength);
+  urlDatabase[key] = "http://" + req.body.longURL;
+
+  res.redirect("/urls/" + key);
+});
+
+// POST /urls/:id/change when we change a URL from urls/:id
+// change the Long URL to point to the new one.
+app.post("/urls/:id/change", (req, res) => {
+  
+  //TODO check parameters verify that req.params.id is valid
+  urlDatabase[req.params.id] = req.body.newLongURL;
+
+  res.redirect("/");
+});
+
+// POST /login - from form in header if user is not logged in
+// Creates cookie for user
+app.post("/login", (req, res) => {
+  //TODO check parameters
+
+  //TODO check that name is not empty
+  res.cookie("username", req.body.userID);
+
+  res.redirect("/urls");
+});
+
 
 app.listen(8080);
 console.log("8080 is the magic port");
