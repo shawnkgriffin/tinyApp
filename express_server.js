c4555b87e6bcbe56cc5340fbf2722b608f74fa75
Shawn Griffin <shawn@shawngriffin.com>
2017-10-10 18:00:24 -0700

    URL Shortening (Part 1)

--- express_server.js
+++ express_server.js
@@ -1,24 +1,49 @@
+//requires and globals
+const bodyParser = require("body-parser");
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+// Temporary database.
 var urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-let templateVars = { urls: urlDatabase };
-for (let item in templateVars.urls) {
-  console.log(item, urlDatabase[item]);
-}
+// Functions and other worker items remember to refactor these to a module.
+
+// generate 6 random alphanumeric characters
+function generateRandomString(length) {
+  const validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let random = "";
+  for (let i = 0; i < length; i++) {
+    random  += validChars[Math.floor(Math.random() * validChars.length)];
+  }
+  return validChars;
+  }
+
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
+
+// use 
+app.use(bodyParser.urlencoded({extended: true}));
+
 // use res.render to load up an ejs view file
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("pages/urls_index", templateVars);
+});
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("pages/urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
